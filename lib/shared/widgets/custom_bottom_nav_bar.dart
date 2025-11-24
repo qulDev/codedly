49d4a23491bf5a9979a09c2284ae322ceadd60ee
Yosa <116474723+VXGN@users.noreply.github.com>
2025-11-24 20:46:24 +0800

    feat : Simplify BottomNavigationBar item tap handling
    Refactored navigation logic in BottomNavigationBar.

--- lib/shared/widgets/custom_bottom_nav_bar.dart
+++ lib/shared/widgets/custom_bottom_nav_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:codedly/core/navigation/routes.dart';
 import 'package:codedly/core/theme/colors.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +10,31 @@ class CustomBottomNavBar extends StatelessWidget {
     required this.currentIndex,
   });
 
+  static const _routes = {
+    0: AppRoutes.home,
+    2: AppRoutes.leaderboard,
+    // ...
+    3: AppRoutes.profile,
+  };
+
+  void _onItemTapped(BuildContext context, int index) {
+    if (index == currentIndex) return;
+    
+    final route = _routes[index];
+    
+    if (route != null) {
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        route,
+        (route) => false,
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Tab ${index + 1} coming soon! 🚀')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
@@ -19,47 +43,13 @@ class CustomBottomNavBar extends StatelessWidget {
       unselectedItemColor: AppColors.textSecondary,
       type: BottomNavigationBarType.fixed,
       currentIndex: currentIndex,
+      onTap: (index) => _onItemTapped(context, index),
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.quiz), label: 'Practice'),
         BottomNavigationBarItem(icon: Icon(Icons.leaderboard), label: 'Leaderboard'),
         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
       ],
-      onTap: (index) {
-        if (index == currentIndex) return;
-        
-        switch (index) {
-          case 0:
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              AppRoutes.home,
-              (route) => false,
-            );
-            break;
-          case 1:
-            // ...
-            break;
-          case 2:
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              AppRoutes.leaderboard,
-              (route) => false,
-            );
-            break;
-          case 3:
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              AppRoutes.profile,
-              (route) => false,
-            );
-            break;
-          default:
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Tab ${index + 1} coming soon! 🚀')),
-          );
-          break;
-        }
-      },
     );
   }
 }
